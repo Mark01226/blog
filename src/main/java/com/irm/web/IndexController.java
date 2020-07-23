80dd9ed0676234e13368c89ceca740c88a5418b9
Mark <942094517@qq.com>
2020-07-23 11:42:14 +0800

    日志处理

---
+++ src/main/java/com/irm/web/IndexController.java
@@ -0,0 +1,21 @@
+package com.irm.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+//    @GetMapping("/{id}/{name}")
+    @GetMapping("/")
+//    public String index(@PathVariable Integer id, @PathVariable String name){
+    public String index(){
+//        int i = 9/0;
+//        String blog = null;
+//        if(blog == null){
+//            throw new NotFoundException("博客不存在");
+//        }
+        System.out.println("------index---------");
+        return "index";
+    }
+}
